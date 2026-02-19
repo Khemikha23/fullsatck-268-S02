81b6bc8b18385cbfddb5b44149beb20457d85077
Khemikha23 <s6706022510310@email.kmutnb.ac.th>
2026-02-19 15:30:19 +0700

    commitlecture09

---
+++ src/MongoDBCRUD.js
@@ -0,0 +1,96 @@
+// Description: REST API with MongoDB
+// npm install express mongoose body-parser
+// Run this file with node MongoDBREST.js
+// Test with Postman
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+// Database connection
+mongoose.connect(
+  "mongodb://admin:XBCgny58611@node86136-env-7545065.th.app.ruk-com.cloud:11839",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const Book = mongoose.model("Book", {
+  id: {
+    type: Number,
+    unique: true, // Ensures uniqueness of the "id" field
+    required: true, // If you want "id" to be required
+  },
+  title: String,
+  author: String,
+});
+
+const app = express();
+app.use(bodyParser.json());
+
+// Create
+app.post("/books", async (req, res) => {
+  try {
+    // Get the last book record to determine the next ID
+    const lastBook = await Book.findOne().sort({ id: -1 });
+    const nextId = lastBook ? lastBook.id + 1 : 1;
+
+    // Create a new book with the next ID
+    const book = new Book({
+      id: nextId, // Set the custom "id" field
+      ...req.body, // Include other book data from the request body
+    });
+
+    await book.save();
+    res.send(book);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Read all
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.send(books);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Read one
+app.get("/books/:id", async (req, res) => {
+  try {
+    const book = await Book.findOne({ id: req.params.id });
+    res.send(book);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Update
+app.put("/books/:id", async (req, res) => {
+  try {
+    const book = await Book.findOneAndUpdate({ id: req.params.id },req.body,{
+        new: true,
+    });
+    res.send(book);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Delete
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const book = await Book.findOneAndDelete({ id: req.params.id });
+    res.send(book);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+ app.listen(port, () => console.log(`Listening on port http://localhost:${port}...`));
